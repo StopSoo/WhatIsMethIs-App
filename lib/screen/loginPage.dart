74d9480092ef3d924604a83ee497226f16035048
220v-K <jwlee010222@gmail.com>
2023-08-22 22:02:32 +0900

    <feat>: Login Page 구현

--- lib/screen/loginPage.dart
+++ lib/screen/loginPage.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application/components/component.dart';
+import 'package:flutter_application/components/decorations.dart';
+import 'package:flutter_application/constants/colors.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -9,13 +12,131 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  String inputEmail = "";
+  String inputPassword = "";
+
   @override
   Widget build(BuildContext context) {
     return safeAreaPage(
       Colors.white,
       Colors.white,
       Column(
-        children: [],
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              CupertinoButton(
+                minSize: 0,
+                padding: const EdgeInsets.only(left: 7.0),
+                child: const Icon(
+                  Icons.arrow_back_ios,
+                  color: bright_green,
+                  size: 27,
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              const Text(
+                "로그인",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                  color: charcoal,
+                ),
+              ),
+              const SizedBox(width: 31),
+            ],
+          ),
+          const SizedBox(height: 144),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 43.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 6.0),
+                      child: Text(
+                        "이메일",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: real_black,
+                        ),
+                      ),
+                    ),
+                    Spacer(),
+                  ],
+                ),
+                CupertinoTextField(
+                  padding: const EdgeInsets.symmetric(vertical: 9, horizontal: 16),
+                  style: const TextStyle(color: CupertinoColors.black),
+                  placeholder: "example@gmail.com",
+                  placeholderStyle: const TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w500,
+                    color: dark_gray,
+                  ),
+                  onChanged: (text) {
+                    setState(() {
+                      inputEmail = text;
+                    });
+                  },
+                  decoration: textFieldDecoration(),
+                ),
+                const SizedBox(height: 20),
+                // 비밀번호
+                const Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 6.0),
+                      child: Text(
+                        "비밀번호",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: real_black,
+                        ),
+                      ),
+                    ),
+                    Spacer(),
+                  ],
+                ),
+                CupertinoTextField(
+                  padding: const EdgeInsets.symmetric(vertical: 9, horizontal: 16),
+                  style: const TextStyle(color: CupertinoColors.black),
+                  placeholder: "********",
+                  placeholderStyle: const TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w500,
+                    color: dark_gray,
+                  ),
+                  onChanged: (text) {
+                    setState(() {
+                      inputPassword = text;
+                    });
+                  },
+                  decoration: textFieldDecoration(),
+                ),
+
+                const SizedBox(height: 20),
+
+                // 비밀번호 찾기 / 회원가입
+                const Padding(
+                  padding: EdgeInsets.only(right: 12.0),
+                  child: Text(
+                    "비밀번호 찾기 | 회원가입",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                      color: charcoal,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
