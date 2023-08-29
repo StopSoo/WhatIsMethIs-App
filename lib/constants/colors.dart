216a9819fba6c415dbe642778c3bf09bd3123e3f
jjunjji <younggy123@gmail.com>
2023-08-29 14:39:41 +0900

    <fix>: colors 추가

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,20 +1,25 @@
 import 'dart:ui';
 
-const Color main_color_green = Color(0xFFD0F5BE);
-const Color main_color_yellow = Color(0xFFFFFCB6);
-const Color bright_green = Color(0xFF2FC955);
-const Color more_green = Color(0xFF6FCD41);
-const Color dark_green = Color(0xFF5EAB39);
-const Color dot_green = Color(0xFFD0F5BE);
-const Color black = Color(0xFF000000);
-const Color white = Color(0xFFFFFFFF);
-const Color gray = Color(0xFFDADADA);
-const Color dark_gray = Color(0xFF6D6D6D);
-const Color bright_gray = Color(0xFFF5F5F5);
-const Color bar_gray = Color(0xFFF2F2F2);
+const main_color_green = Color(0xFFD0F5BE);
+const main_color_yellow = Color(0xFFFFFCB6);
+const bright_yellow = Color(0xFFFFD50B);
+const bright_green = Color(0xFF2FC955);
+const more_green = Color(0xFF6FCD41);
+const dark_green = Color(0xFF5EAB39);
+const dark_yellow = Color(0xFF5EAB39);
+const dot_green = Color(0xFFD0F5BE);
+const charcoal = Color(0xFF272727);
+const black = Color(0xFF000000);
+const white = Color(0xFFFFFFFF);
+const gray = Color(0xFFDADADA);
+const dark_gray = Color(0xFF6D6D6D);
+const bright_gray = Color(0xFFF5F5F5);
+const bar_gray = Color(0xFFF2F2F2);
 
 // font
 const String mainFont = 'Inter-Medium';
+
+const Color real_black = Color.fromRGBO(0, 0, 0, 1);
 
 const Color redColor = Color(0xFFFF6161); 
 
